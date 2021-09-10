cfca6ea32390e04d9fd6ffebd141f3a9f848ab51
BoniFederico <67047565+BoniFederico@users.noreply.github.com>
2021-09-10 13:09:22 +0200

    Update compRadialDistortion.m

--- Project/compRadialDistortion.m
+++ Project/compRadialDistortion.m
@@ -20,28 +20,33 @@ for ii=1:size(imageData,2)
         v0=imageData(ii).K(2,3);
         u_=imageData(ii).XYpixel(jj,1);
         v_=imageData(ii).XYpixel(jj,2);
+        
+        u_=imageData(ii).true_proj(jj,1);
+        v_=imageData(ii).true_proj(jj,2);
+        
+        
         comp_proj_hom=imageData(ii).P*transpose([imageData(ii).XYmm(jj,:),0,1]);
         u=comp_proj_hom(1)/comp_proj_hom(3);
         v=comp_proj_hom(2)/comp_proj_hom(3);
         skew=acotd(imageData(ii).K(1,2)/imageData(ii).K(1,1));
         alpha_u=imageData(ii).K(1,1);
         alpha_v=imageData(ii).K(2,2)*sin(skew);
-        Rd=sqrt(((u-u0)/alpha_u)^2+((v-v0)/alpha_v)^2);
-        
+        Rd2=((u-u0)/alpha_u)^2+((v-v0)/alpha_v)^2;
         A=[ A
-            (u-u0)*Rd^2, (u-u0)*Rd^4
-            (v-v0)*Rd^2  (v-v0)*Rd^4 ];
+            (u-u0)*Rd2, (u-u0)*Rd2^2
+            (v-v0)*Rd2,  (v-v0)*Rd2^2 ];
         b= [ b; (u_ -u) ; (v_ -v) ];
         
     end
 end
 k=inv((transpose(A)*A))*transpose(A)*b; %using least squares
-
+disp(k);
 %------------------BREAK IF K1,K2 < THRESHOLD-----------------------------
 if (norm(k-kold)<threshold)
     for ii=1:size(imageData,2)
         imageData(ii).k=k;
-        imageData(ii).distortionModel=@(u,v) [(u-u0)*(1+k(1)*Rd^2+k(2)*Rd^4)+u0, (v-v0)*(1+k(1)*Rd^2+k(2)*Rd^4)+v0];
+        computeRd2 = @(u,v) ((u-u0)/alpha_u)^2+((v-v0)/alpha_v)^2;
+        imageData(ii).distortionModel=@(u,v) [(u-u0)*(1+k(1)*computeRd2(u,v)+k(2)*computeRd2(u,v)^2)+u0, (v-v0)*(1+k(1)*computeRd2(u,v)+k(2)*computeRd2(u,v)^2)+v0];
         dist_reproj_errors=[];
         for jj=1:size(imageData(ii).XYpixel,1)
             %true_proj=imageData(ii).XYpixel(jj,:);
@@ -50,6 +55,7 @@ if (norm(k-kold)<threshold)
             comp_proj=[comp_proj_hom(1)/comp_proj_hom(3);comp_proj_hom(2)/comp_proj_hom(3)];
             comp_proj=imageData(ii).distortionModel(comp_proj(1),comp_proj(2));
             dist_reproj_errors=[dist_reproj_errors;norm(true_proj-comp_proj)];
+            
         end
         imageData(ii).dist_reproj_errors=mean(dist_reproj_errors);
         
@@ -61,21 +67,21 @@ end
 
 %-------------------COMPUTE NEW COORDINATES U,V----------------------------
 
-newImageData=[];
-newImageData.I=imageData.I;
-newImageData.XYmm=imageData.XYmm;
 for ii=1:size(imageData,2)
     
     for jj=1:size(imageData(ii).XYpixel,1)
         u0=imageData(ii).K(1,3);
         v0=imageData(ii).K(2,3);
-        u_=imageData(ii).XYpixel(jj,1);
-        v_=imageData(ii).XYpixel(jj,2);
+        
+        u_=imageData(ii).true_proj(jj,1);
+        v_=imageData(ii).true_proj(jj,2);
+        
+        
+        
         alpha_u=imageData(ii).K(1,1);
         alpha_v=imageData(ii).K(2,2)/sin(skew);
         x_=(u_ -u0)/alpha_u;
         y_=(v_ -v0)/alpha_v;
-        
         f=@(x) [x(1)*(1+k(1)*(x(1)^2+x(2)^2)+k(2)*(x(1)^4+2*x(1)^2*x(2)^2+x(2)^4))-x_,x(2)*((1+k(1)*(x(1)^2+x(2)^2)+k(2)*(x(1)^4+2*x(1)^2*x(2)^2+x(2)^4)))-y_];
         options = optimset('Display','off');
         res=fsolve(f,[x_,y_],options);
